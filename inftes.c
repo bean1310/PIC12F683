c7701b1ac989664d66fac1a8267613f631b33bab
bean1310 <1310isato@gmail.com>
2017-08-01 17:10:13 +0900

    ver.1

--- inftes.c
+++ inftes.c
@@ -3,15 +3,15 @@
 #include "rpi-pin.h"
 
 int main(){
+  int i = 0;
   if(wiringPiSetup() == -1) return 1;
   pinMode(15, INPUT);
   while(1){
-      if(digitalRead(15) == HIGH){
-        printf("HIGH\n");
-      }else{
-        printf("LOW\n");
-      }
-      delay(5);
+	if(digitalRead(15) == LOW) i++;
+	if(i == 3){
+	  printf("OKOKOKOK\n");
+	}
+      delayMicroseconds(25);
     }
   return 0;
 }
